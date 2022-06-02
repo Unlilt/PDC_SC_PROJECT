6131a66a2cf2fd47885e2dcc9f92d3e61019fa3d
Jade Thompson-Tavai <jhp4379@aut.ac.nz>
2022-06-02 12:32:39 +1200

    Attempt to implement GUI - Components not being located properly - All lining up at the top - Attempt to use CardLayout - When I ddid nothing appeared on JFrame

--- PDC_SC_PROJECT/src/game/GUI.java
+++ PDC_SC_PROJECT/src/game/GUI.java
@@ -7,12 +7,14 @@ package game;
 
 
 
-import java.awt.BorderLayout;
-import java.awt.Color;
+import java.awt.CardLayout;
 import java.awt.Dimension;
-import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /**
  *
@@ -20,22 +22,99 @@ import javax.swing.JPanel;
  */
 public class GUI extends JPanel {
     
-    private final DrawPanel drawPanel;
+//    private final DrawPanel drawPanel;
     JFrame window;
-
+    private CardLayout cLayout;
+    private  JButton loadGameBtn;
+    private  JButton newGameBtn;
+    private  JLabel titleLabel;
+    private  JPanel titlePanel;
+    private  JPanel panelCont = new JPanel();
+    
     public GUI(){
-       drawPanel = new DrawPanel();
-       JPanel panel = new JPanel();
-       panel.add(drawPanel);
+         try {
+            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+        } catch (UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        } catch (IllegalAccessException ex) {
+            ex.printStackTrace();
+        } catch (InstantiationException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        /* Turn off metal's use of bold fonts */
+        UIManager.put("swing.boldMetal", Boolean.FALSE);
+        setLayout(null);
+        cLayout = new CardLayout();
+        initComponents();
        
-       super.add(panel, BorderLayout.CENTER);
+        
+//        
+//        titlePanel.add(newGameBtn);
+//        titlePanel.add(loadGameBtn);
+//        titlePanel.add(titleLabel);
+//        titlePanel.setVisible(true);
+//        
+//        super.add(titlePanel);
+        
+        
+        
     }
     
-     public class DrawPanel extends JPanel {
-        public DrawPanel()
-        {
-            super.setPreferredSize(new Dimension(600,600));
-            super.setBackground(Color.BLACK);
-        }
+//     public class DrawPanel extends JPanel {
+//        public DrawPanel()
+//        {
+//            super.setPreferredSize(new Dimension(800,600));
+//            super.setBackground(Color.BLACK);
+//        }
+    
+    //Main method for testing
+    public static void main(String[] args) {
+          GUI gameGui = new GUI();
+            //Create/initialize GUI
+            JFrame window = new JFrame("Evil Emperor Oz Rising!");
+            window.setPreferredSize(new Dimension(800, 600));
+            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            window.setContentPane(gameGui);
+            window.pack();
+            window.setVisible(true);
+    }
+
+    private void initComponents() {
+        titlePanel = new JPanel();
+        titlePanel.setBounds(0, 0, 800, 600);
+        newGameBtn = new JButton();
+        loadGameBtn = new JButton();
+        titleLabel = new JLabel();
+
+        panelCont.setLayout(cLayout);
+        
+        newGameBtn.setText("New Game");
+        newGameBtn.setLocation(100, 700);
+        newGameBtn.setVisible(true);
+
+        loadGameBtn.setText("Load Game");
+        loadGameBtn.setVisible(true);
+        
+        
+        titleLabel.setAlignmentX(TOP_ALIGNMENT);
+        titleLabel.setAlignmentY(TOP_ALIGNMENT);
+        titleLabel.setFont(new java.awt.Font("Dialog", 0, 24)); // NOI18N
+        titleLabel.setText("OZIRIS RISING");
+        titleLabel.setVisible(true);
+        
+//        
+        titlePanel.add(newGameBtn);
+        titlePanel.add(loadGameBtn);
+        titlePanel.add(titleLabel);
+        titlePanel.setVisible(true);
+        
+        panelCont.add(titlePanel, "1");
+//        cLayout.show(panelCont, "1");
+        
+        super.add(titlePanel);    
+    }
 }
-}
+
